8709525648ed692f5de92955e4058d1c24bff404
andres alcocer <devandres.tech@gmail.com>
2022-08-11 17:52:23 -0400

    create .gitignore

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+var cors = require('cors')
+var app = express()
+var proxy = require('express-http-proxy')
+var axios = require('axios')
+
+app.use(cors())
+
+// app.use(
+//   '/getmakes',
+//   proxy('https://www.gasbuddy.com/tripcostcalculator/getmakes')
+// )
+
+app.get('/getmakes', (req, res, next) => {
+  var config = {
+    method: 'get',
+    url: 'https://www.gasbuddy.com/tripcostcalculator/getmakes',
+    headers: {
+      Authorization: 'Bearer',
+      Cookie:
+        'datadome=i~Ini44wxx~ISKoHrNrYi9w_wYE9wdvFu.wlP9mAoTwV9tsr_VWqDFcEOFkZ.cI1yDBX5rdd07Cwe2n4jQ9.taeymdg_sqGyBvLgX4~0cTxgaVa_Kz2dF8wXevBuN5Q; ASP.NET_SessionId=1icrwangnfi0hx0rlfl5vz14',
+    },
+  }
+
+  axios(config)
+    .then(function (response) {
+      return res.send(response.data)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+})
+
+app.listen(8000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
