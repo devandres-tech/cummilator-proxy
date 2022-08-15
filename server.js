1b9a0e89c2fe03aaf29303a3bafe36f4ef8b50af
andres alcocer <devandres.tech@gmail.com>
2022-08-15 14:07:25 -0400

    implemented price api

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 var cors = require('cors')
 var app = express()
-var proxy = require('express-http-proxy')
 var axios = require('axios')
 
 app.use(cors())
@@ -84,6 +83,65 @@ app.get('/getvehiclespec', (req, res, next) => {
   axios(config)
     .then(function (response) {
       return res.status(200).send(response.data)
+    })
+    .catch(function (error) {
+      return res.send(error)
+    })
+})
+
+app.get('/getgasprice', (req, res, next) => {
+  const { search } = req.query
+  if (!search)
+    return res.status(404).send({ error: 'query param "search" is missing' })
+
+  var data = JSON.stringify({
+    operationName: 'LocationBySearchTerm',
+    variables: {
+      fuel: 1,
+      maxAge: 0,
+      search: search,
+    },
+    query:
+      'query LocationBySearchTerm($brandId: Int, $cursor: String, $fuel: Int, $lat: Float, $lng: Float, $maxAge: Int, $search: String) {\n  locationBySearchTerm(lat: $lat, lng: $lng, search: $search) {\n    countryCode\n    displayName\n    latitude\n    longitude\n    regionCode\n    stations(brandId: $brandId, cursor: $cursor, fuel: $fuel, maxAge: $maxAge) {\n      count\n      cursor {\n        next\n        __typename\n      }\n      results {\n        address {\n          country\n          line1\n          line2\n          locality\n          postalCode\n          region\n          __typename\n        }\n        badges {\n          badgeId\n          callToAction\n          campaignId\n          clickTrackingUrl\n          description\n          detailsImageUrl\n          detailsImpressionTrackingUrls\n          imageUrl\n          impressionTrackingUrls\n          targetUrl\n          title\n          __typename\n        }\n        brandings {\n          brand_id\n          branding_type\n          __typename\n        }\n        brands {\n          brand_id\n          image_url\n          name\n          __typename\n        }\n        emergency_status {\n          has_diesel {\n            nick_name\n            report_status\n            update_date\n            __typename\n          }\n          has_gas {\n            nick_name\n            report_status\n            update_date\n            __typename\n          }\n          has_power {\n            nick_name\n            report_status\n            update_date\n            __typename\n          }\n          __typename\n        }\n        enterprise\n        fuels\n        id\n        name\n        offers {\n          discounts {\n            grades\n            pwgb_discount\n            __typename\n          }\n          types\n          __typename\n        }\n        pay_status {\n          is_pay_available\n          __typename\n        }\n        prices {\n          cash {\n            nickname\n            posted_time\n            price\n            __typename\n          }\n          credit {\n            nickname\n            posted_time\n            price\n            __typename\n          }\n          discount\n          fuel_product\n          __typename\n        }\n        ratings_count\n        star_rating\n        __typename\n      }\n      __typename\n    }\n    trends {\n      areaName\n      country\n      today\n      todayLow\n      trend\n      __typename\n    }\n    __typename\n  }\n}\n',
+  })
+
+  var config = {
+    method: 'post',
+    url: 'https://www.gasbuddy.com/graphql',
+    headers: {
+      authority: 'www.gasbuddy.com',
+      accept: '*/*',
+      'accept-language': 'en-US,en;q=0.9,es-MX;q=0.8,es;q=0.7,la;q=0.6',
+      'content-type': 'application/json',
+    },
+    data: data,
+  }
+
+  axios(config)
+    .then(function (response) {
+      let priceResults =
+        response.data.data.locationBySearchTerm.stations.results
+          .map((item) => {
+            return item.prices.filter((item) => item.credit.price > 0)[0]
+          })
+          .filter((item) => item !== undefined)
+
+      const sumTotal = priceResults.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.credit.price
+      }, 0)
+      const locationAverage = parseFloat(
+        (sumTotal / priceResults.length).toFixed(2)
+      )
+
+      const locationAverageDisplay = (
+        Math.round(locationAverage * Math.pow(10, 2)) / Math.pow(10, 2)
+      ).toFixed(2)
+
+      return res.status(200).send({
+        locationAverage,
+        locationAverageDisplay,
+        location: response.data.data.locationBySearchTerm.displayName,
+      })
     })
     .catch(function (error) {
       return res.send(error)
